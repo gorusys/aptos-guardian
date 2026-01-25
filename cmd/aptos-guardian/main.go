f52890e475e0a6ac96c09ebe3a03bd764cd921ee
gorusys <goru.connector@outlook.com>
2026-01-25 20:49:24 +0700

    fix: gitignore binary only, add cmd/aptos-guardian

---
+++ cmd/aptos-guardian/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorusys/aptos-guardian/internal/util/version"
+)
+
+func main() {
+	configPath := flag.String("config", "configs/example.yaml", "Path to YAML config")
+	showVersion := flag.Bool("version", false, "Print version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		for k, v := range version.Info() {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+		os.Exit(0)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigCh
+		cancel()
+	}()
+
+	if err := run(ctx, *configPath); err != nil && ctx.Err() == nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, configPath string) error {
+	f, err := os.Open(configPath)
+	if err != nil {
+		return fmt.Errorf("config: %w", err)
+	}
+	_ = f.Close()
+	<-ctx.Done()
+	return nil
+}
